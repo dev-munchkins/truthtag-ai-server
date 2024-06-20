58d73b10eafc0d451520029418b0d683216f6ec6
HyunKyungHan <hhk@ewhain.net>
2024-06-20 22:38:21 +0900

    add: ai module code

--- ai/models/chatbot_model.py
+++ ai/models/chatbot_model.py
@@ -1,8 +1,128 @@
-def chatbot(sentence: str):
+# def chatbot(sentence: str):
 
-    result = {
-        "product": "오뚜기 컵누들",
-        "function": 1
-    }
+#     result = {
+#         "product": "오뚜기 컵누들",
+#         "function": 1
+#     }
 
-    return result
+#     return result
+# BERT 모델에 넣을 dataset
+from KoBERT.kobert import get_tokenizer
+from KoBERT.kobert import get_pytorch_kobert_model
+# basic dependencies
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+# import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm
+
+# data processing dependencies
+import pandas as pd
+
+# transformers
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+import gluonnlp as nlp
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') 
+bertmodel, vocab = get_pytorch_kobert_model()
+# tokenizer = get_tokenizer()
+# tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower = False)
+
+max_len = 64
+batch_size = 32
+warmup_ratio = 0.1
+num_epochs = 20
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-05
+
+class BERTDataset(Dataset):
+  def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len, pad, pair):
+    transform = nlp.data.BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+    self.sentences = [transform([i[sent_idx]]) for i in dataset]
+    self.labels = [np.int32(i[label_idx]) for i in dataset]
+    # print(self.sentences)
+    # print(self.labels)
+
+  def __getitem__(self, i):
+    return (self.sentences[i] + (self.labels[i], ))
+
+  def __len__(self):
+    return (len(self.labels))
+
+# # 데이터 토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower = False)
+
+# data_train = BERTDataset(dataset_train, 0, 1, tok , max_len, True, False)
+# data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
+
+# train_dataloader = torch.utils.data.DataLoader(data_train, batch_size = batch_size, num_workers = 5)
+# test_dataloader = torch.utils.data.DataLoader(data_test, batch_size = batch_size, num_workers = 5)
+
+# define classifier
+# num_classes : 분류하고자 하는 class 수
+class BERTClassifier(nn.Module):
+  def __init__(self, bert, hidden_size = 768, num_classes = 5, dr_rate=None, params=None):
+    super(BERTClassifier, self).__init__()
+    self.bert = bert
+    self.dr_rate = dr_rate
+
+    self.classifier = nn.Linear(hidden_size, num_classes)
+    if dr_rate:
+      self.dropout = nn.Dropout(p=dr_rate)
+
+  def gen_attention_mask(self, token_ids, valid_length):
+    attention_mask = torch.zeros_like(token_ids)
+    for i, v in enumerate(valid_length):
+      attention_mask[i][:v] = 1
+    return attention_mask.float()
+
+  def forward(self, token_ids, valid_length, segment_ids):
+    attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+    _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+    if self.dr_rate:
+      out = self.dropout(pooler)
+    return self.classifier(out)
+  
+model = BERTClassifier(bertmodel, dr_rate = 0.5).to(device)
+model.load_state_dict(torch.load('../weights/chat_model.pt'))
+
+category = {'슈링크플레이션': 0 , '스킴플레이션': 1, '원재료유사품': 2, '즐겨찾기': 3, '2가지상품비교': 4}
+
+def test(predict_sentence):
+  data = [predict_sentence, '0']
+  dataset_another = [data]
+  another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+  test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers = 5)
+
+  model.eval()
+  for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+    token_ids = token_ids.long().to(device)
+    segment_ids = segment_ids.long().to(device) # Move segment_ids to device
+    valid_length = valid_length
+    label = label.long().to(device)
+    # token_ids = token_ids.long()
+    # segment_ids = segment_ids.long() # Move segment_ids to device
+    # valid_length = valid_length
+    # label = label.long()
+
+    out = model(token_ids, valid_length, segment_ids)
+    test_eval = []
+    for i in out:
+      logits = i
+      logits = logits.detach().cpu().numpy()
+      test_eval.append(list(category.keys())[(np.argmax(logits))])
+
+    result = dict()
+    result['function'] = test_eval[0]
+    print('Inference 결과:', test_eval[0])
+    return(test_eval[0])
+
+if __name__ == '__main__':
+  test('이거 두 개를 비교해줘.')
--- ai/models/ocr_model.py
+++ ai/models/ocr_model.py
@@ -1,8 +1,104 @@
-def ocr_tag(image_url):
+# from core.util.image_util import load_image
 
-    result = {
-        "brand": "오뚜기",
-        "product": "컵누들"
-    }
+import torch
+import cv2
+import os
+import re
+import numpy as np
+import shutil
+from easyocr import Reader
+from rapidfuzz import fuzz
 
-    return result
+def ocr_tag(img_url):
+    new_img = cv2.imread(img_url)
+
+    # Run yolov5
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='..\\weights\\ocr_weights_2.pt', force_reload=True)
+    results = model(new_img)
+
+    # save cropped area
+    results.crop()
+    # cur_dir = os.getcwd()
+    crop_path = 'runs\\detect\\exp\\crops\\words\\image0.jpg'
+    crop_img = cv2.imread(crop_path)
+
+    # run easyOCR in cropped
+    langs = ['ko', 'en']
+    print("[INFO] OCR'ing input image...")
+    reader = Reader(lang_list=langs, gpu=True)
+    results = reader.readtext(crop_img)
+
+    real = []
+    for (bbox, text, prob) in results:
+      pot = [')', '>', '"\"', '원', '#', ","]
+      # if ')' in text or '>' in text or '"\"' in text or '원' in text:
+      for i in pot:
+        if i in text:
+          real.append(text)
+      (tl, tr, br, bl) = bbox
+      tl = (int(tl[0]), int(tl[1]))
+      tr = (int(tr[0]), int(tr[1]))
+      br = (int(br[0]), int(br[1]))
+      bl = (int(bl[0]), int(bl[1]))
+    
+    #  cv2.rectangle(org_image, tl, br, (0, 255, 0), 3)
+    info = dict()
+    brand = ''
+    product = ''
+    price = ''
+    for i in real:
+        if ')' in i:
+            brand = i.split(')')[0]
+            product = i.split(')')[1]
+            info['brand'] = brand
+            info['product'] = product
+        if '>' in i:
+            price = i.split('>')[-1]
+            info['price'] = price
+        if "'\'" in i:
+            price = i.split("'\'")[-1]
+            info['price'] = price
+        if '#' in i:
+            price = i.split("#")[-1]
+            info['price'] = price
+        if "원" in i:
+            price = i.split("원")[0]
+            info['price'] = price
+        if "," in i:
+            price = str(i)
+            info['price'] = price
+        if "00" in i:
+            price = str(i)
+            info['price'] = price
+    
+    product_list = ["신라면", "신라면블랙", "진라면", "안성탕면", "나가사끼짬뽕", "카구리", "너구리", "불닭볶음면",  "컵누들우동맛", "컵누들매운맛", "비빔면큰컵", "새우깡", "매운새우깡", "꼬깔콘", "포카칩" , "초코파이", "빵부장", "초코하임", "화이트하임", "메로나", "누가바"]
+    brand_list = ["매일", "농심", "빙그레", "서울우유", "롯데", "크라운", "팔도", "오리온", "오뚜기", "리츠"]
+
+    for i in info:
+        info[i] = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", '', info[i])
+
+    idxi, idxj = -1, -1
+    max_brand, max_product = 0.0, 0.0
+    
+    # if len(info['brand']) == 0:
+    for i in brand_list:
+        prob = fuzz.ratio(i, info['brand'])
+        if np.float16(prob) > np.float16(max_brand):
+            max_brand = prob
+            idxi = i
+
+    # if len(info['product']) != 0:
+    for j in product_list:
+        prob2 = fuzz.ratio(j, info['product'])
+        if (np.float16(prob2) > np.float16(max_product)):
+            max_product = prob2
+            idxj = j
+
+    info['brand'] = idxi
+    info['product'] = idxj
+
+    shutil.rmtree('runs/detect')
+    print('ocr process done.')
+    print('detected:', info)
+    # 출력 예시 : {'brand': '롯데', 'product': ' 제로후르츠젤리52g', 'price': '1200'}
+    return info
